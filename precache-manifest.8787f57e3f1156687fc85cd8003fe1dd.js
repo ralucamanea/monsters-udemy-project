d7724047d82673ab29ae306956ed368edb98aca2
Raluca Manea <raluca96ok@gmail.com>
2020-05-19 17:49:20 +0200

    Updates

---
+++ precache-manifest.8787f57e3f1156687fc85cd8003fe1dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272da6e2dd984eb8393cdca83ed9234b",
+    "url": "/monsters-udemy-project/index.html"
+  },
+  {
+    "revision": "ac173656971749d883fb",
+    "url": "/monsters-udemy-project/static/css/main.394a3bf2.chunk.css"
+  },
+  {
+    "revision": "258a3b1518a475fb3df7",
+    "url": "/monsters-udemy-project/static/js/2.57f35528.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-udemy-project/static/js/2.57f35528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac173656971749d883fb",
+    "url": "/monsters-udemy-project/static/js/main.cf9543b8.chunk.js"
+  },
+  {
+    "revision": "1e31e6b9dee40d8e0c30",
+    "url": "/monsters-udemy-project/static/js/runtime-main.2e21b308.js"
+  }
+]);
